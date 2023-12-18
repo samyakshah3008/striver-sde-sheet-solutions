48d894415b3b53260afa8d312809610e5be699b0
Samyak Shah <samyakshah3008@gmail.com>
2023-12-18 23:14:16 +0530

    pattern 1

---
+++ Step1/Step1.2/pq1.js
@@ -0,0 +1,14 @@
+// Pattern to print:
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+for (i = 0; i < 4; i++) {
+  for (j = 0; j < 4; j++) {
+    console.log("*");
+  }
+  console.log("\n");
+}
