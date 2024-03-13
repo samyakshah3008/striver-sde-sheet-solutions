fe383ca2199f80e88b7c181f70d4af65359b62fa
Samyak Shah <samyakshah3008@gmail.com>
2024-03-13 23:33:14 +0530

    minor update

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-for (i = 0; i < 5; i++) {
-  let str = "";
-  for (j = 1; j <= 2 * i + 1; j++) {
-    str += "*";
-  }
-  console.log(str);
-}
+// const printUpside = (rows) => {
+//   for (let i = 1; i <= rows; i++) {
+//     let str = "";
+//     let spaceCount = 2 * i - 2;
+
+//     for (let j = rows; j >= 1; j--) {
+//       str += "*";
+//     }
+
+//     for (let k = 1; k <= spaceCount; k++) {
+//       str += "-";
+//     }
+
+//     for (let l = rows; l >= 1; l--) {
+//       str += "*";
+//     }
+
+//     console.log(str);
+//   }
+// };
+
+// printUpside(5);
+
+const multiplyBy5 = (num) => num * 5;
+multiplyBy5.power = 2;
+console.log(multiplyBy5(5));
+console.log(multiplyBy5.power);
+console.log(multiplyBy5.prototype);
