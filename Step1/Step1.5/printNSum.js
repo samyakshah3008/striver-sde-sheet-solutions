de86b436db9e3da8b8cf9ffc38b3540210d28bff
Samyak Shah <samyakshah3008@gmail.com>
2024-03-19 22:11:05 +0530

    add: print n sum using recursion sol

---
+++ Step1/Step1.5/printNSum.js
@@ -0,0 +1,26 @@
+let accumulator = 0;
+const sum = (n, index = 1) => {
+  if (index <= n) {
+    accumulator = accumulator + index;
+    index++;
+    sum(n, index);
+  } else {
+    console.log(accumulator);
+    return accumulator;
+  }
+};
+
+console.log(sum(5));
+
+function sumNaturalNumbers(n) {
+  // Base case: stop recursion when n is 0
+  if (n === 0) {
+    return 0; // Sum of 0 natural numbers is 0
+  }
+
+  // Recursively compute the sum of natural numbers from 1 to n
+  return n + sumNaturalNumbers(n - 1);
+}
+
+const n = 5;
+console.log("Sum of the first", n, "natural numbers is:", sumNaturalNumbers(n));
