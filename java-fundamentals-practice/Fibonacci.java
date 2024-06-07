d98536c16c65a4813e1d117f1824c0e0cd3d6437
Samyak Shah <samyakshah3008@gmail.com>
2024-06-07 22:52:44 +0530

    fibonacci with java

---
+++ java-fundamentals-practice/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int previous = 0;
+        int current = 1;
+
+        for (int count = 0; count <= 10; count++) {
+            int temp = current;
+            current = current + previous;
+            previous = temp;
+            System.out.println(current);
+        }
+
+        System.out.println(current);
+    }
+}
