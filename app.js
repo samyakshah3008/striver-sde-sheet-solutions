9d417a38be157eb9aa67ef65c3477193ed7f1f83
Samyak Shah <samyakshah3008@gmail.com>
2024-06-07 22:52:55 +0530

    main files

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-// const printUpside = (rows) => {
-//   for (let i = 1; i <= rows; i++) {
-//     let str = "";
-//     let spaceCount = 2 * i - 2;
-
-//     for (let j = rows; j >= 1; j--) {
-//       str += "*";
-//     }
-
-//     for (let k = 1; k <= spaceCount; k++) {
-//       str += "-";
-//     }
-
-//     for (let l = rows; l >= 1; l--) {
-//       str += "*";
-//     }
-
-//     console.log(str);
-//   }
+// let count = 1;
+// const print1ToNUsingBacktrack = (endNumber) => {
+//   if (count > endNumber) return;
+//   count++;
+//   print1ToNUsingBacktrack(endNumber);
+//   console.log(count);
 // };
 
-// printUpside(5);
+// print1ToNUsingBacktrack(5);
 
-const multiplyBy5 = (num) => num * 5;
-multiplyBy5.power = 2;
-console.log(multiplyBy5(5));
-console.log(multiplyBy5.power);
-console.log(multiplyBy5.prototype);
+// function memoize(fn) {
+//   let cachedValue;
+//   console.log(cachedValue);
+//   return function (...args) {
+//     cachedValue = fn(...args);
+//     return cachedValue;
+//   };
+// }
+
+// let callCount = 0;
+// const memoizedFn = memoize(function (a, b) {
+//   callCount += 1;
+//   return a + b;
+// });
+// memoizedFn(2, 3); // 5
+// memoizedFn(2, 3); // 5
+// console.log(callCount); // 1
+
+// const object = { a: "1", b: "2" };
+// console.log(Object.keys(object).length, "here");
+// const secondObj = [null];
+// console.log(Object.keys(secondObj).length, "second obj");
+
+// var isEmpty = function (obj) {
+//   if (!Object.keys(obj).length) return true;
+//   return false;
+// };
+
+function userCreator(name, score) {
+  const newUser = {};
+  newUser.name = name;
+  newUser.score = score;
+  newUser.increment = function () {
+    newUser.score++;
+  };
+
+  return newUser;
+}
+
+const user1 = userCreator("Will", 3);
+user1.increment();
+const user2 = userCreator("Will2", 5);
+console.log(user1.score);
+console.log(user2.score);
