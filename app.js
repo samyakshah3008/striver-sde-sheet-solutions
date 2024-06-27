f4c9f3c3d1e4345a6e2ceadd4bb69068d5dc0580
Samyak Shah <samyakshah3008@gmail.com>
2024-06-27 16:48:00 +0530

    minor update

--- app.js
+++ app.js
@@ -1,54 +1,21 @@
-// let count = 1;
-// const print1ToNUsingBacktrack = (endNumber) => {
-//   if (count > endNumber) return;
-//   count++;
-//   print1ToNUsingBacktrack(endNumber);
-//   console.log(count);
-// };
+var timeLimit = (fn, t) => {
+  return async (...args) => {};
+};
 
-// print1ToNUsingBacktrack(5);
+const t = 50;
+const fn = async (n) => {
+  await new Promise((res) => setTimeout(res, 100));
+  return n * n;
+};
 
-// function memoize(fn) {
-//   let cachedValue;
-//   console.log(cachedValue);
-//   return function (...args) {
-//     cachedValue = fn(...args);
-//     return cachedValue;
-//   };
-// }
-
-// let callCount = 0;
-// const memoizedFn = memoize(function (a, b) {
-//   callCount += 1;
-//   return a + b;
-// });
-// memoizedFn(2, 3); // 5
-// memoizedFn(2, 3); // 5
-// console.log(callCount); // 1
-
-// const object = { a: "1", b: "2" };
-// console.log(Object.keys(object).length, "here");
-// const secondObj = [null];
-// console.log(Object.keys(secondObj).length, "second obj");
-
-// var isEmpty = function (obj) {
-//   if (!Object.keys(obj).length) return true;
-//   return false;
-// };
-
-function userCreator(name, score) {
-  const newUser = {};
-  newUser.name = name;
-  newUser.score = score;
-  newUser.increment = function () {
-    newUser.score++;
-  };
-
-  return newUser;
+const limited = timeLimit(fn, t);
+const start = performance.now();
+let result;
+let inputs = [5];
+try {
+  const res = await limited(...inputs);
+  result = { resolved: res, time: Math.floor(performance.now() - start) };
+} catch (err) {
+  result = { rejected: err, time: Math.floor(performance.now() - start) };
 }
-
-const user1 = userCreator("Will", 3);
-user1.increment();
-const user2 = userCreator("Will2", 5);
-console.log(user1.score);
-console.log(user2.score);
+console.log(result); // Output
