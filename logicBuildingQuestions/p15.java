793d91c4854d3f2b383925699dd25963bf3f7a4d
Samyak Shah <samyakshah3008@gmail.com>
2024-06-28 22:13:18 +0530

    p15 solve

---
+++ logicBuildingQuestions/p15.java
@@ -0,0 +1,15 @@
+package logicBuildingQuestions;
+
+public class p15 {
+    public static void main(String[] args) {
+        System.out.println("welcome");
+
+        for (int i = 1; i <= 30; i++) {
+            if (i <= 10 || i >= 20) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
