78121fec77312482d27dcca5e23075c1c1370f85
Samyak Shah <samyakshah3008@gmail.com>
2024-06-29 00:02:42 +0530

    p27 solve

---
+++ logicBuildingQuestions/p27.java
@@ -0,0 +1,30 @@
+package logicBuildingQuestions;
+
+import java.util.Scanner;
+
+public class p27 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number which you want to reverse");
+        int number = scanner.nextInt();
+        // eg 1234
+        int reversedNumber = reverse(number);
+
+        System.out.println(reversedNumber);
+        scanner.close();
+
+    }
+
+    public static int reverse(int number) {
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number = number / 10;
+        }
+
+        return reversed;
+    }
+}
