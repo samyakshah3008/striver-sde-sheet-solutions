f8196996e5a06eaef4b27d765adea7354b97d763
Samyak Shah <samyakshah3008@gmail.com>
2024-06-28 21:05:15 +0530

    question 2 solved

---
+++ logicBuildingQuestions/p2.java
@@ -0,0 +1,34 @@
+package logicBuildingQuestions;
+
+import java.util.Scanner;
+
+public class p2 {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter two numbers
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Perform basic arithmetic operations
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplication = num1 * num2;
+        double division = num1 / num2;
+        double modulo = num1 % num2;
+
+        // Display the results
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+        System.out.println("Modulo: " + modulo);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
