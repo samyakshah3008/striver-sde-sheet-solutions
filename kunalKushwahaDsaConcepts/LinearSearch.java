9d85ecc54f524606401f5a45d94a125088618d52
Samyak Shah <samyakshah3008@gmail.com>
2024-07-29 23:11:26 +0530

    feat: linear search algo sol add

--- kunalKushwahaDsaConcepts/LinearSearch.java
+++ kunalKushwahaDsaConcepts/LinearSearch.java
@@ -21,11 +21,31 @@ public class LinearSearch {
         return -1;
     }
 
+    public static boolean linearSearchString(String sampleString, char target) {
+
+        if (sampleString.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < sampleString.length(); i++) {
+            if (sampleString.charAt(i) == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println("Linear search. ");
 
         int[] sampleArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         int elementToSearch = 17;
+
+        String sampleString = "Samyak";
+        char toSearchCharacter = 'a';
+
+        boolean ansString = linearSearchString(sampleString, toSearchCharacter);
+        System.out.println(ansString);
 
         int ans = linearSearch(sampleArray, elementToSearch);
         System.out.println(ans);
